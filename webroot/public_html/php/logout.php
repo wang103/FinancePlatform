8696e108cc29aa05bf1b63247fbae9487fc91548
Tianyi Wang <wang103uiuc@gmail.com>
2013-06-29 12:21:56 -0500

    Log out finished

---
+++ webroot/public_html/php/logout.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+# Unset the varibales stored in the session.
+unset($_SESSION['EMAIL']);
+unset($_SESSION['FIRST_NAME']);
+unset($_SESSION['LAST_NAME']);
+unset($_SESSION['STATUS']);
+
+# Go back.
+$last_url = $_SESSION['last_url'];
+header("location: " . $last_url);
+die();
+?>
