ba2b99a932030eb736d3f8a917ed031ee1abebdf
Tianyi Wang <wang103uiuc@gmail.com>
2013-07-01 09:15:23 -0500

    minor modification

--- webroot/public_html/php/show_announcement.php
+++ webroot/public_html/php/show_announcement.php
@@ -1,6 +1,8 @@
 <h3>公告栏</h3>
 
 <?php
+session_start();
+
 # Connect to the database.
 require('../config.php');
 
@@ -22,11 +24,17 @@ while ($row = mysql_fetch_array($result)) {
     $content . '<br><br>' .
     '<i>' . $row['date'] . ' by ' . $row['poster'] . '</i>' .
     '</p>';
+    
+    # If it's professor, allow modifying the post.
+    if (isset($_SESSION['STATUS']) && $_SESSION['STATUS'] == 0) {
+        echo '
+        <input type="button" value="修改" onClick="location=\'php/modify_announcement.php\';"/>
+        <input type="button" value="删除" onClick="location=\'php/delete_announcement.php\';"/>
+        ';
+    }
+    
     echo '<hr>';
     $announce_empty = 0;
-
-    # If it's professor, allow modifying the post.
-
 }
 
 if ($announce_empty == 1) {
@@ -46,7 +54,7 @@ if (isset($_SESSION['STATUS']) && $_SESSION['STATUS'] == 0) {
     <hr style="height:5px;">
     <form action="php/insert_announcement.php" method="post">
     <p>
-    公告内容：<br>
+    新公告内容：<br>
     <textarea name="content" rows="6" cols="60" required></textarea>
     <br>
     <input type="submit" value="发布新公告"/>
