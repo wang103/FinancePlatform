834969fe450aa688ae902846ff6f5dcbbc749f71
Tianyi Wang <wang103uiuc@gmail.com>
2013-07-02 06:26:59 -0500

    Delete announcement done

---
+++ webroot/public_html/php/delete_announcement.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+require('../../config.php');
+
+$con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+if (!$con) {
+    die('Could not connect: ' . mysql_error());
+}
+mysql_select_db(DB_DATABASE, $con);
+
+# Remove the announcement from the table.
+$announce_id = $_GET['an'];
+
+$qry = 'DELETE FROM announcements WHERE announce_id="' . $announce_id . '"';
+mysql_query($qry);
+
+mysql_close($con);
+
+header('location: ../index.php');
+die();
+?>
