90571816dba4e64259a5fac28f3e5a94f8484fa6
Tianyi Wang <wang103uiuc@gmail.com>
2013-07-03 07:33:20 -0500

    in middle of submit

--- webroot/public_html/php/create_new_request.php
+++ webroot/public_html/php/create_new_request.php
@@ -1,6 +1,29 @@
 <?php
-# Check user signed in and user name.
+session_start();
+
+# Check user signed in.
+if (!isset($_SESSION['EMAIL']) || empty($_SESSION['EMAIL'])) {
+    echo 'error code: 0';
+    die();
+}
+
+# Check user name.
+if (($_SESSION['LAST_NAME'] . $_SESSION['FIRST_NAME']) != $_POST['name']) {
+    echo 'error code: 1';
+    die();
+}
 
 # Check if there is budget.
+if ($_POST['budget'] != "yes") {
+    echo 'error code: 2';
+    die();
+}
+
+# Check if files are complete.
+if ($_POST['files'] != "yes") {
+    echo 'error code: 3';
+    die();
+}
+
 
 ?>
--- webroot/public_html/php/show_check.php
+++ webroot/public_html/php/show_check.php
@@ -1,1 +1,25 @@
 <h3>我的报销申请</h3>
+
+未完成的报销
+<table border="1">
+    <tr>
+        <th>报销时间</th>
+        <th>报销人</th>
+        <th>金额</th>
+        <th>报销科目</th>
+        <th>申请状态</th>
+    </tr>
+</table>
+
+<br>
+
+已完成的报销
+<table border="1">
+    <tr>
+        <th>报销时间</th>
+        <th>报销人</th>
+        <th>金额</th>
+        <th>报销科目</th>
+        <th>申请状态</th>
+    </tr>
+</table>
