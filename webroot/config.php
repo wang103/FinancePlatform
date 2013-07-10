cf1778aded9de91b4c55da9213c988190432b5a4
Tianyi Wang <wang103uiuc@gmail.com>
2013-07-10 00:05:11 -0500

    Added my server info

--- webroot/config.php
+++ webroot/config.php
@@ -4,6 +4,13 @@ define(DB_DATABASE, 'reimbursement_db');
 define(DB_USER, 'root');
 define(DB_PASSWORD, 'tianyi');
 
+/*
+define(DB_HOST, '72.167.233.37');
+define(DB_DATABASE, 'reimbursement');
+define(DB_USER, 'reimbursement');
+define(DB_PASSWORD, 'Tianyi001!');
+*/
+
 define(SEND_EMAIL, false);
 
 define(INTEL_PLATFORM_URL, 'http://www.google.com');
