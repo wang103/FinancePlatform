c9838bdb7ccc41270ab04bdf97e271c39cd2844a
Tianyi Wang <wang103uiuc@gmail.com>
2013-07-11 05:37:43 -0500

    Fall back option for bad sql is added

--- webroot/public_html/php/modify_account.php
+++ webroot/public_html/php/modify_account.php
@@ -61,8 +61,18 @@ if (!mysql_query($qry, $con)) {
 # Update advisor.
 if ($_SESSION['STATUS'] != 0 && $_SESSION['STATUS'] != 3) {
     # Student.
-    $qry = 'UPDATE advisors SET advisor_email="' . $advisor_email .
-        '" WHERE student_email="' . $email . '"';
+    $chk_qry = 'SELECT * FROM advisors WHERE student_email="' .
+        $_SESSION['EMAIL'] . '"';
+    $chk_result = mysql_query($chk_qry, $con);
+
+    if (mysql_num_rows($chk_result) == 0) {
+        $qry = 'UPDATE advisors SET advisor_email="' . $advisor_email .
+            '" WHERE student_email="' . $email . '"';
+    } else {
+        $qry = 'UPDATE advisors SET student_email="' .
+            $email . '", advisor_email="' . $advisor_email .
+            '" WHERE student_email="' . $_SESSION['EMAIL'] . '"';
+    }
 
     if (!mysql_query($qry, $con)) {
         die('Error: ' . mysql_error());
@@ -72,8 +82,25 @@ if ($_SESSION['STATUS'] != 0 && $_SESSION['STATUS'] != 3) {
 # Update rows in requests.
 if ($_SESSION['STATUS'] != 0 && $_SESSION['STATUS'] != 3) {
     # Student.
-    $qry = 'UPDATE requests SET financial_assistant_name="' . $last_name . $first_name .
-        '" WHERE financial_assistant_email="' . $email . '"';
+    $chk_qry = 'SELECT * FROM requests WHERE financial_assistant_email="' .
+        $_SESSION['EMAIL'] . '" OR transfered_email="' . $_SESSION['EMAIL'] .
+        '"';
+    $chk_result = mysql_query($chk_qry, $con);
+
+    if (mysql_num_rows($chk_result) == 0) {
+        $qry = 'UPDATE requests SET financial_assistant_name="' . $last_name . $first_name .
+            '" WHERE financial_assistant_email="' . $email . '"';
+    } else {
+        $qry = 'UPDATE requests SET financial_assistant_email="' . $email .
+            '", financial_assistant_name="' . $last_name . $first_name .
+            '" WHERE financial_assistant_email="' . $_SESSION['EMAIL'] . '"';
+
+        $additional_qry = 'UPDATE requests SET transfered_email="' . $email .
+            '" WHERE transfered_email="' . $_SESSION['EMAIL'] . '"';
+        if (!mysql_query($additional_qry, $con)) {
+            die('Error: ' . mysql_error());
+        }
+    }
 
     if (!mysql_query($qry, $con)) {
         die('Error: ' . mysql_error());
@@ -83,8 +110,18 @@ if ($_SESSION['STATUS'] != 0 && $_SESSION['STATUS'] != 3) {
 # Update rows in announcements.
 if ($_SESSION['STATUS'] == 0 || $_SESSION['STATUS'] == 3) {
     # Professor.
-    $qry = 'UPDATE announcements SET poster="' . $last_name . $first_name .
-        '" WHERE poster_email="' . $email . '"';
+    $chk_qry = 'SELECT * FROM announcements WHERE poster_email="' .
+        $_SESSION['EMAIL'] . '"';
+    $chk_result = mysql_query($chk_qry, $con);
+
+    if (mysql_num_rows($chk_result) == 0) {
+        $qry = 'UPDATE announcements SET poster="' . $last_name . $first_name .
+            '" WHERE poster_email="' . $email . '"';
+    } else {
+        $qry = 'UPDATE announcements SET poster_email="' . $email .
+            '", poster="' . $last_name . $first_name .
+            '" WHERE poster_email="' . $_SESSION['EMAIL'] . '"';
+    }
 
     if (!mysql_query($qry, $con)) {
         die('Error: ' . mysql_error());
