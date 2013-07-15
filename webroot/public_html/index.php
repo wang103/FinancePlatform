04575d4a1127cc6b1f2d2a645e65d7253d5215f1
Tianyi Wang <wang103uiuc@gmail.com>
2013-07-15 09:56:31 -0500

    Added nyan cat

--- webroot/public_html/index.php
+++ webroot/public_html/index.php
@@ -9,6 +9,24 @@ session_start();
 <title>北邮报销申请 - 主页</title>
 <link rel='stylesheet' type='text/css' href='css/style00.css'>
 <script src='js/utils.js'></script>
+
+<script>
+var isPlaying = false;
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+        var nyanElem = document.getElementById("nyan_div");
+        if (isPlaying) {
+            nyanElem.style.display = 'none';
+            isPlaying = false;
+        } else {
+            nyanElem.style.display = 'block';
+            isPlaying = true;
+        }
+    }
+};
+</script>
+
 </head>
 
 <body>
@@ -20,6 +38,12 @@ $_SESSION['last_url'] = $temp[0];
 ?>
 
 <h1>北邮报销申请 - 主页</h1>
+
+<div id='nyan_div' style="display: none">
+<object data="video/nyan.swf" height="500" width="500">
+<embed scr="video/nyan.swf" height="500" width="500">
+</object>
+</div>
 
 <div id='container'>
 
