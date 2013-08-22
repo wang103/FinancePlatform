4e74fcb05c3b7058de6f1b9894e87a9c64f5e6c6
Tianyi Wang <wang103uiuc@gmail.com>
2013-08-21 17:29:20 -0700

    Minor

--- webroot/config.php
+++ webroot/config.php
@@ -3,7 +3,7 @@
  * Database connection information.
  */
 define(DB_HOST, 'localhost');
-define(DB_DATABASE, 'reimbursement_db');
+define(DB_DATABASE, 'reimbursement');
 define(DB_USER, 'root');
 define(DB_PASSWORD, 'tianyi');
 
